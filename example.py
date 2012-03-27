0bbbe9777dfbeb6adb2eade2984501c0bd3bb144
Tomo Krajina <tkrajina@gmail.com>
2012-03-27 06:54:55 +0200

    Example simplified

--- example.py
+++ example.py
@@ -6,191 +6,214 @@ import math as mod_math
 
 mod_logging.basicConfig( level = mod_logging.DEBUG, format = '%(asctime)s %(name)-12s %(levelname)-8s %(message)s' )
 
+examples = []
 
-class Test:
+def test_lines():
+	"""Lines of different colors"""
+	coordinate_system = mod_cartesius.CoordinateSystem()
 
-	def test_010_lines( self ):
-		"""Lines of different colors"""
-		coordinate_system = mod_cartesius.CoordinateSystem()
+	coordinate_system.add( mod_cartesius.Line( ( 0, 0 ), ( -.7, -.7 ) ) )
+	coordinate_system.add( mod_cartesius.Line( ( .5, -.5 ), ( -.5, .5 ), color = ( 0, 255, 0 ) ) )
+	coordinate_system.add( mod_cartesius.Line( ( 0, 0 ), ( 10, 1 ), color = ( 0, 0, 255 ) ) )
 
-		coordinate_system.add( mod_cartesius.Line( ( 0, 0 ), ( -.7, -.7 ) ) )
-		coordinate_system.add( mod_cartesius.Line( ( .5, -.5 ), ( -.5, .5 ), color = ( 0, 255, 0 ) ) )
-		coordinate_system.add( mod_cartesius.Line( ( 0, 0 ), ( 10, 1 ), color = ( 0, 0, 255 ) ) )
+	return coordinate_system.draw( 600, 300, show_labels = True )
 
-		return coordinate_system.draw( 600, 300, show_labels = True )
+examples.append( test_lines )
 
-	def test_020_function( self ):
-		""" Function math.sin from -4 to 5"""
-		coordinate_system = mod_cartesius.CoordinateSystem()
+def test_function():
+	""" Function math.sin from -4 to 5"""
+	coordinate_system = mod_cartesius.CoordinateSystem()
 
-		f = lambda x : mod_math.sin( x ) * 2
-		coordinate_system.add( mod_cartesius.GraphFunction( f, start = -4, end = 5, step = 0.02, color = ( 0, 0, 255 ) ) )
+	f = lambda x : mod_math.sin( x ) * 2
+	coordinate_system.add( mod_cartesius.GraphFunction( f, start = -4, end = 5, step = 0.02, color = ( 0, 0, 255 ) ) )
 
-		return coordinate_system.draw( 600, 300, show_labels = True )
+	return coordinate_system.draw( 600, 300, show_labels = True )
 
-	def test_030_filled_function( self ):
-		""" Line function and normal function but with filled graph"""
-		coordinate_system = mod_cartesius.CoordinateSystem()
+examples.append( test_function )
 
-		f = lambda x : mod_math.sin( x ) * 2
-		coordinate_system.add( mod_cartesius.GraphFunction( f, start = -4, end = 5, step = 0.02, color = ( 0, 0, 255 ) ) )
+def test_filled_function():
+	""" Line function and normal function but with filled graph"""
+	coordinate_system = mod_cartesius.CoordinateSystem()
 
-		g = lambda x : mod_math.sin( x ) * 2
-		coordinate_system.add( mod_cartesius.GraphFunction( g, start = 1, end = 4, step = 0.02, fill_color = ( 200, 255, 200 ) ) )
+	f = lambda x : mod_math.sin( x ) * 2
+	coordinate_system.add( mod_cartesius.GraphFunction( f, start = -4, end = 5, step = 0.02, color = ( 0, 0, 255 ) ) )
 
-		return coordinate_system.draw( 600, 300, show_labels = True )
+	g = lambda x : mod_math.sin( x ) * 2
+	coordinate_system.add( mod_cartesius.GraphFunction( g, start = 1, end = 4, step = 0.02, fill_color = ( 200, 255, 200 ) ) )
 
-	def test_040_filled_function( self ):
-		""" Previous example with grid behind graphs """
-		coordinate_system = mod_cartesius.CoordinateSystem()
+	return coordinate_system.draw( 600, 300, show_labels = True )
 
-		# Grid:
-		coordinate_system.add( mod_cartesius.Grid( 1, 1 ) )
+examples.append( test_filled_function )
 
-		f = lambda x : mod_math.sin( x ) * 2
-		coordinate_system.add( mod_cartesius.GraphFunction( f, start = -4, end = 5, step = 0.02, color = ( 0, 0, 255 ) ) )
+def test_filled_function():
+	""" Previous example with grid behind graphs """
+	coordinate_system = mod_cartesius.CoordinateSystem()
 
-		g = lambda x : mod_math.sin( x ) * 2
-		coordinate_system.add( mod_cartesius.GraphFunction( g, start = 1, end = 4, step = 0.02, fill_color = ( 200, 255, 200 ) ) )
+	# Grid:
+	coordinate_system.add( mod_cartesius.Grid( 1, 1 ) )
 
-		return coordinate_system.draw( 600, 300, show_labels = True )
+	f = lambda x : mod_math.sin( x ) * 2
+	coordinate_system.add( mod_cartesius.GraphFunction( f, start = -4, end = 5, step = 0.02, color = ( 0, 0, 255 ) ) )
 
-	def test_050_filled_transparent_graphs( self ):
-		""" Two functions """
-		coordinate_system = mod_cartesius.CoordinateSystem()
+	g = lambda x : mod_math.sin( x ) * 2
+	coordinate_system.add( mod_cartesius.GraphFunction( g, start = 1, end = 4, step = 0.02, fill_color = ( 200, 255, 200 ) ) )
 
-		coordinate_system.add(
-				mod_cartesius.GraphFunction(
-						mod_math.sin,
-						start = -4,
-						end = 5,
-						step = 0.02,
-						fill_color = ( 0, 0, 255 ),
-						transparency_mask = 100 ) )
+	return coordinate_system.draw( 600, 300, show_labels = True )
 
-		coordinate_system.add(
-				mod_cartesius.GraphFunction(
-						mod_math.cos,
-						start = -4,
-						end = 5,
-						step = 0.02,
-						fill_color = ( 200, 255, 200 ),
-						transparency_mask = 100 ) )
+examples.append( test_filled_function )
 
-		return coordinate_system.draw( 600, 300, show_labels = True )
+def test_filled_transparent_graphs():
+	""" Two functions """
+	coordinate_system = mod_cartesius.CoordinateSystem()
 
-	def test_055_filled_transparent_graphs( self ):
-		""" Two functions with transparend grid over it """
-		coordinate_system = mod_cartesius.CoordinateSystem()
+	coordinate_system.add(
+			mod_cartesius.GraphFunction(
+					mod_math.sin,
+					start = -4,
+					end = 5,
+					step = 0.02,
+					fill_color = ( 0, 0, 255 ),
+					transparency_mask = 100 ) )
 
-		coordinate_system.add(
-				mod_cartesius.GraphFunction(
-						mod_math.sin,
-						start = -4,
-						end = 5,
-						step = 0.02,
-						fill_color = ( 0, 0, 255 ),
-						transparency_mask = 100 ) )
+	coordinate_system.add(
+			mod_cartesius.GraphFunction(
+					mod_math.cos,
+					start = -4,
+					end = 5,
+					step = 0.02,
+					fill_color = ( 200, 255, 200 ),
+					transparency_mask = 100 ) )
 
-		coordinate_system.add(
-				mod_cartesius.GraphFunction(
-						mod_math.cos,
-						start = -4,
-						end = 5,
-						step = 0.02,
-						fill_color = ( 200, 255, 200 ),
-						transparency_mask = 100 ) )
+	return coordinate_system.draw( 600, 300, show_labels = True )
 
-		coordinate_system.add( mod_cartesius.Grid( 1, 1, transparency_mask = 140 ) )
+examples.append( test_filled_transparent_graphs )
 
-		return coordinate_system.draw( 600, 300, show_labels = True )
+def test_filled_transparent_graphs_2():
+	""" Two functions with transparend grid over it """
+	coordinate_system = mod_cartesius.CoordinateSystem()
 
-	def test_060_key_value_graphs( self ):
-		""" Key-value graphs"""
-		coordinate_system = mod_cartesius.CoordinateSystem()
+	coordinate_system.add(
+			mod_cartesius.GraphFunction(
+					mod_math.sin,
+					start = -4,
+					end = 5,
+					step = 0.02,
+					fill_color = ( 0, 0, 255 ),
+					transparency_mask = 100 ) )
 
-		# With dict:
-		coordinate_system.add(
-				mod_cartesius.KeyValueGraph(
-						values = { -2: 1, 0: -1, 3: 1.2, 7: 1.2 },
-						fill_color = ( 50, 50, 50 ),
-						transparency_mask = 50 ) )
-		# With pairs of tuples
-		coordinate_system.add( 
-				mod_cartesius.KeyValueGraph( 
-						values = ( ( 0, 0 ), ( 1, -3 ), ( 10, 3 ) ),
-						color = ( 255, 0, 0 ),
-						transparency_mask = 150 ) )
+	coordinate_system.add(
+			mod_cartesius.GraphFunction(
+					mod_math.cos,
+					start = -4,
+					end = 5,
+					step = 0.02,
+					fill_color = ( 200, 255, 200 ),
+					transparency_mask = 100 ) )
 
-		return coordinate_system.draw( 600, 300, show_labels = True )
+	coordinate_system.add( mod_cartesius.Grid( 1, 1, transparency_mask = 140 ) )
 
-	def test_070_circles( self ):
-		""" Key-value graphs"""
-		coordinate_system = mod_cartesius.CoordinateSystem()
+	return coordinate_system.draw( 600, 300, show_labels = True )
 
-		coordinate_system.add(
-			mod_cartesius.Circle(
-				x = 4,
-				y = -3,
-				radius = 2,
-				transparency_mask = 200,
-				fill_color = ( 150, 150, 150 ),
-				color = ( 0, 0, 0 ) ) )
-		coordinate_system.add(
-			mod_cartesius.Circle(
-				x = 4,
-				y = 3,
-				radius = 2,
-				transparency_mask = 200,
-				color = ( 0, 0, 255 ) ) )
+examples.append( test_filled_transparent_graphs_2 )
 
-		return coordinate_system.draw( 600, 300, show_labels = True )
+def test_key_value_graphs():
+	""" Key-value graphs"""
+	coordinate_system = mod_cartesius.CoordinateSystem()
 
-	def test_080_circles_2( self ):
-		""" Another example with circles """
-		coordinate_system = mod_cartesius.CoordinateSystem()
-		
-		for i in range( 1, 20 ):
-			x = i / 2.
-			coordinate_system.add( mod_cartesius.Circle( x, y = mod_math.sin( x ), radius = mod_math.sqrt( x ), transparency_mask = 50, fill_color = ( i * 10, 2 * 10, i * 10 ), color = ( 0, 0, 0 ) ) )
+	# With dict:
+	coordinate_system.add(
+			mod_cartesius.KeyValueGraph(
+					values = { -2: 1, 0: -1, 3: 1.2, 7: 1.2 },
+					fill_color = ( 50, 50, 50 ),
+					transparency_mask = 50 ) )
+	# With pairs of tuples
+	coordinate_system.add( 
+			mod_cartesius.KeyValueGraph( 
+					values = ( ( 0, 0 ), ( 1, -3 ), ( 10, 3 ) ),
+					color = ( 255, 0, 0 ),
+					transparency_mask = 150 ) )
 
-		return coordinate_system.draw( 600, 300, show_labels = True )
+	return coordinate_system.draw( 600, 300, show_labels = True )
 
-	def test_090_circles( self ):
-		""" Circles with horizontal grid """
-		coordinate_system = mod_cartesius.CoordinateSystem()
-		
-		coordinate_system.add( mod_cartesius.Grid( 1, None, transparency_mask = 200 ) )
+examples.append( test_key_value_graphs )
 
-		for i in range( 1, 20 ):
-			x = i / 2.
-			coordinate_system.add( mod_cartesius.Circle( x, y = mod_math.sin( x ), radius = mod_math.sqrt( x ), transparency_mask = 50, fill_color = ( i * 10, 2 * 10, i * 10 ), color = ( 0, 0, 0 ) ) )
+def test_circles():
+	""" Key-value graphs"""
+	coordinate_system = mod_cartesius.CoordinateSystem()
 
-		return coordinate_system.draw( 600, 300, show_labels = True )
+	coordinate_system.add(
+		mod_cartesius.Circle(
+			x = 4,
+			y = -3,
+			radius = 2,
+			transparency_mask = 200,
+			fill_color = ( 150, 150, 150 ),
+			color = ( 0, 0, 0 ) ) )
+	coordinate_system.add(
+		mod_cartesius.Circle(
+			x = 4,
+			y = 3,
+			radius = 2,
+			transparency_mask = 200,
+			color = ( 0, 0, 255 ) ) )
 
-	def test_090_circles( self ):
-		""" Circles with horizontal grid every 2 """
-		coordinate_system = mod_cartesius.CoordinateSystem()
-		
-		coordinate_system.add( mod_cartesius.Grid( 2, None, transparency_mask = 200 ) )
+	return coordinate_system.draw( 600, 300, show_labels = True )
 
-		for i in range( 1, 20 ):
-			x = i / 2.
-			coordinate_system.add( mod_cartesius.Circle( x, y = mod_math.sin( x ), radius = mod_math.sqrt( x ), transparency_mask = 50, fill_color = ( i * 10, 2 * 10, i * 10 ), color = ( 0, 0, 0 ) ) )
+examples.append( test_circles )
 
-		return coordinate_system.draw( 600, 300, show_labels = True )
+def test_circles_2():
+	""" Another example with circles """
+	coordinate_system = mod_cartesius.CoordinateSystem()
+	
+	for i in range( 1, 20 ):
+		x = i / 2.
+		coordinate_system.add( mod_cartesius.Circle( x, y = mod_math.sin( x ), radius = mod_math.sqrt( x ), transparency_mask = 50, fill_color = ( i * 10, 2 * 10, i * 10 ), color = ( 0, 0, 0 ) ) )
 
-	def test_100_circles( self ):
-		""" Circles with vertical grid every 0.5 """
-		coordinate_system = mod_cartesius.CoordinateSystem()
-		
-		coordinate_system.add( mod_cartesius.Grid( None, 0.5, transparency_mask = 200 ) )
+	return coordinate_system.draw( 600, 300, show_labels = True )
 
-		for i in range( 1, 20 ):
-			x = i / 2.
-			coordinate_system.add( mod_cartesius.Circle( x, y = mod_math.sin( x ), radius = mod_math.sqrt( x ), transparency_mask = 50, fill_color = ( i * 10, 2 * 10, i * 10 ), color = ( 0, 0, 0 ) ) )
+examples.append( test_circles_2 )
 
-		return coordinate_system.draw( 600, 300, show_labels = True )
+def test_circles_3():
+	""" Circles with horizontal grid """
+	coordinate_system = mod_cartesius.CoordinateSystem()
+	
+	coordinate_system.add( mod_cartesius.Grid( 1, None, transparency_mask = 200 ) )
+
+	for i in range( 1, 20 ):
+		x = i / 2.
+		coordinate_system.add( mod_cartesius.Circle( x, y = mod_math.sin( x ), radius = mod_math.sqrt( x ), transparency_mask = 50, fill_color = ( i * 10, 2 * 10, i * 10 ), color = ( 0, 0, 0 ) ) )
+
+	return coordinate_system.draw( 600, 300, show_labels = True )
+
+examples.append( test_circles_3 )
+
+def test_circles_4():
+	""" Circles with horizontal grid every 2 """
+	coordinate_system = mod_cartesius.CoordinateSystem()
+	
+	coordinate_system.add( mod_cartesius.Grid( 2, None, transparency_mask = 200 ) )
+
+	for i in range( 1, 20 ):
+		x = i / 2.
+		coordinate_system.add( mod_cartesius.Circle( x, y = mod_math.sin( x ), radius = mod_math.sqrt( x ), transparency_mask = 50, fill_color = ( i * 10, 2 * 10, i * 10 ), color = ( 0, 0, 0 ) ) )
+
+	return coordinate_system.draw( 600, 300, show_labels = True )
+
+examples.append( test_circles_4 )
+
+def test_circles_5():
+	""" Circles with vertical grid every 0.5 """
+	coordinate_system = mod_cartesius.CoordinateSystem()
+	
+	coordinate_system.add( mod_cartesius.Grid( None, 0.5, transparency_mask = 200 ) )
+
+	for i in range( 1, 20 ):
+		x = i / 2.
+		coordinate_system.add( mod_cartesius.Circle( x, y = mod_math.sin( x ), radius = mod_math.sqrt( x ), transparency_mask = 50, fill_color = ( i * 10, 2 * 10, i * 10 ), color = ( 0, 0, 0 ) ) )
+
+	return coordinate_system.draw( 600, 300, show_labels = True )
+
+examples.append( test_circles_5 )
 
 if __name__ == '__main__':
 	html = """<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
@@ -204,24 +227,16 @@ if __name__ == '__main__':
 	</head>
 	<body>"""
 
-	test = Test()
+	for i, function in enumerate( examples ):
+		description = function.__doc__.strip()
+		image = function()
+		image_name = 'graph-{0}.png'.format( i )
+		image.save( image_name )
 
-	elements = dir( test )
-	elements.sort()
-	for method in elements:
-		if method.startswith( 'test_' ):
-			name = method.replace( 'test_', '' )
+		html += '<h2>{0}:</h2>'.format( description )
+		html += '<p><img src="{0}" /></p>'.format( image_name )
 
-			method = getattr( test, method )
-			description = method.__doc__
-			image = method()
-			image_name = '{0}.png'.format( name )
-			image.save( image_name )
-
-			html += '<h2>{0}:</h2>'.format( description )
-			html += '<p><img src="{0}" /></p>'.format( image_name )
-
-			print 'written:', image_name
+		print 'written:', image_name
 
 	html += '</body>'
 
