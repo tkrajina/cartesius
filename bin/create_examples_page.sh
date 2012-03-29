c8e97f67747998194e738c5d9f1454ebef175720
Tomo Krajina <tkrajina@gmail.com>
2012-03-29 21:33:50 +0200

    Examples

--- bin/create_examples_page.sh
+++ bin/create_examples_page.sh
@@ -1,4 +1,10 @@
 python example.py
+tar -cvf examples.tar *png *html
+rm *png *html
+mv examples.tar /tmp/
 git checkout gh-pages
+mv /tmp/examples.tar .
+tar -xvf examples.tar
+rm *tar
 git add *.png *.html
 git commit --amend -m "Examples"
