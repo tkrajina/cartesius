18c4a716f5404abc4529fa599bf6a2ae24022140
Tomo Krajina <tkrajina@gmail.com>
2012-04-15 08:11:38 +0200

    tmp

---
+++ cartesius/charts.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+""" Charts are normal CoordinateSystemElements """
+
+import main as mod_main
+
+class Histogram( mod_main.CoordinateSystemElement ):
+
+	def __init__( self ):
+		mod_main.CoordinateSystemElements.__init__( self )
